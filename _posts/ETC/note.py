289dddd24b2e9add9e54d3770f9b959470688eca
1yoouoo <1yoouoo@gmail.com>
2021-03-07 16:35:55 +0900

    counting sort

--- _posts/ETC/note.py
+++ _posts/ETC/note.py
@@ -1,15 +1,26 @@
 N = int(input())
-lists = []
-for i in range(N):
-    i = float(input())
-    lists.append(i)
+A = []
+for x in range(N):
+    x = int(input())
+    A.append(x)
 
-# 산술평균
-aver = sum(lists)/N
-print(round(aver,1))
-# 중앙값
-print(sorted(lists)[round(N/2,1)])
-# 최빈 값
-print(round(sorted(lists)[1],1))
-#범위
-print(round(max(lists)-min(lists),1))
+def couting_sort(A, k):
+    B = [0] * len(A)
+    C = [0] * (k+1)
+
+    for i in range(len(A)): # 각 element가 몇개있는지 C에 저장한다
+        C[A[i]] += 1
+    
+    for i in range(1,len(C)): # C를 누적값으로 바꾼다
+        C[i] += C[i-1]
+
+    for i in range(len(A)): # C를 indexing해서 B에 저장한다
+        B[C[A[i]]-1] = A[i]
+        C[A[i]] -= 1
+
+    return B
+        
+count = couting_sort(A, max(A))
+
+for i in count:
+    print(i)
