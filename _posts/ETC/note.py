1e7c48787f57c1690cef8ffffdf189284b2deaf7
1yoouoo <1yoouoo@gmail.com>
2021-03-07 19:13:44 +0900

    ddd

--- _posts/ETC/note.py
+++ _posts/ETC/note.py
@@ -1,26 +1,6 @@
 N = int(input())
-A = []
-for x in range(N):
-    x = int(input())
-    A.append(x)
+lists = sorted(list(str(N)))
+print(lists)
 
-def couting_sort(A, k):
-    B = [0] * len(A)
-    C = [0] * (k+1)
-
-    for i in range(len(A)): # 각 element가 몇개있는지 C에 저장한다
-        C[A[i]] += 1
-    
-    for i in range(1,len(C)): # C를 누적값으로 바꾼다
-        C[i] += C[i-1]
-
-    for i in range(len(A)): # C를 indexing해서 B에 저장한다
-        B[C[A[i]]-1] = A[i]
-        C[A[i]] -= 1
-
-    return B
-        
-count = couting_sort(A, max(A))
-
-for i in count:
-    print(i)
+for i in range(len(lists)):
+    print(lists[-i-1],end='')
