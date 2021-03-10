2a36e909a560cfed1c9aeac74d6c8b43423d1798
1yoouoo <1yoouoo@gmail.com>
2021-03-10 17:53:01 +0900

    Update note2.py

--- _posts/ETC/note2.py
+++ _posts/ETC/note2.py
@@ -1,7 +1,13 @@
-nums = input()
-nums = [int(n)  for n in nums]
+member_num = int(input())
+member_list = []
 
-ordered_nums = sorted(nums, reverse=True)
+for _ in range(member_num):
+    member_age, member_name = map(str, input().split())
+    member_age = int(member_age)
+    member_list.append((member_age, member_name))
 
-for n in ordered_nums : 
-    print(n, end="")
+#나이 숫자 정렬 > 가입순 정렬
+member_list.sort(key = lambda member: (member[0]))
+
+for member in member_list:
+    print(member[0], member[1])
