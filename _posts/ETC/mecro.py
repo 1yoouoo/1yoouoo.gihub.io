9e5cb584421096d3ff943bd095940f22955e7e13
Blanc <1yoouoo@gmail.com>
2021-03-13 19:04:59 +0900

    Update mecro.py

--- _posts/ETC/mecro.py
+++ _posts/ETC/mecro.py
@@ -1,8 +1,27 @@
-import pyautogui
+import pyautogui as pag
 import time
 
-while True:
-    time.sleep(2)
-    pyautogui.click()
 
 
+def image_serching(URL):
+    pag.moveTo(1231, 59)
+    time.sleep(1)
+    pag.click()
+    pag.write(URL)
+    pag.press('enter')
+    while True:
+        find_img = pag.locateOnScreen('vscode.png')
+        if find_img==None:
+            time.sleep(3)
+        else:
+            break
+    center = pag.center(image)
+    pag.click(center)
+
+image_serching('https://code.visualstudio.com/')
+
+
+
+
+
+# pyautogui.press('enter', presses=3, interval=3)
